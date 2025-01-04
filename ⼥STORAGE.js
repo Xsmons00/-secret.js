2a1593af32d92753f9c989fe7cf5b00e2792f4f4
Xsmons00 <xsmonshost@gmail.com>
2025-01-04 22:47:59 +0700

    Add files via upload

---
+++ ⼥STORAGE.js
@@ -0,0 +1,8 @@
+// secret.js
+module.exports = {
+  GITHUB_DATABASE_URL: "https://raw.githubusercontent.com/putraleomeko/config.json/main/apikey.json",
+  GITHUB_TOKEN: "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",
+  REPO_OWNER: "putraleomeko",
+  REPO_NAME: "config.json",
+  FILE_PATH: "apikey.json",
+};
